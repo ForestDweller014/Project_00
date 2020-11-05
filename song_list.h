9db69222f7220ae55ec69e724cb9e55192eeeab9
Alex Zhang <71724195+Kappachino-Latte@users.noreply.github.com>
2020-11-05 18:36:12 -0500

    Update song_list.h

--- song_list.h
+++ song_list.h
@@ -1,3 +1,30 @@
-struct song_node {char name[100]; char artist[100]; struct song_node *next;};
-struct song_node *insert_alphabetical(struct song_node *curr_list, char *name, char *artist);
-void print_list(struct song_node *curr_list);
+#ifndef SONG_H
+#define SONG_H
+
+struct song_node {
+	char name[100]; 
+	char artist[100]; 
+	struct song_node *next;
+};
+
+struct song_node *new_song(char *name, char *artist);
+
+struct song_node *insert_front (struct song_node *, char*, char*);
+
+struct song_node *insert_alphabetical (struct song_node *curr_list, char *name, char *artist);
+
+void print_song (struct song_node *);
+
+void print_list (struct song_node *curr_list);
+
+void free_song (struct song_node *);
+
+struct song_node *find_artist_song (struct song_node *, char *, char *);
+
+struct song_node *find_first_artist_song (struct song_node *, char *);
+
+struct song_node *delete_song (struct song_node *, char *);
+
+struct song_node *free_list (struct song_node *);
+
+#endif
