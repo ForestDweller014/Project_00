cc73c50dac2400a17d233ba2c3fe1b5e08b55c49
Alex Zhang <71724195+Kappachino-Latte@users.noreply.github.com>
2020-11-05 18:34:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "song_array.h"
 
 int main() {
+	/*
 	struct song_node *library[27];
 	int i;
 	for (i = 0; i < 27; i++) {
@@ -21,5 +22,38 @@ int main() {
 	add_song(library, "Sicko Mode", "Travis Scott");
 	add_song(library, "Black", "*Grizzly*");
 	print_library(library);
+	*/
+	
+	printf ("Testing insert_front, insert_alphabetical, print_list\n------------------------\n");
+	struct song_node *library = NULL;
+	library = insert_front (library, "Take What You Want", "Post Malone");
+	library = insert_front (library, "While My Guitar Gently Weeps", "George Harrison");
+	library = insert_alphabetical (library, "Test Dummy", " Post Malone");
+	library = insert_alphabetical (library, "Soothsayer", "Buckethead");
+	printf("\nPrinting List\n\n");
+	print_list (library);
+	printf("\n");
+	
+	printf ("Testing find_artist_song\n------------------------\n");
+	find_artist_song(library, "Chosen One", "Polo G");
+	printf("\n");
+	find_artist_song(library, "Take What You Want", "Post Malone");
+	printf("\n");
+	
+	printf ("Testing find_first_artist_song\n------------------------\n");
+	find_first_artist_song(library, "Polo G");
+	printf("\n");
+	find_first_artist_song(library, "Post Malone");	
+	printf("\n");
+	
+	printf ("Testing delete_song\n------------------------\n");
+	delete_song (library, "Test Dummy");
+	printf("\n");
+	print_list (library);
+	printf("\n");
+	
+	library = free_list (library);
+	printf("\n");
+	
 	return 0;
 }
