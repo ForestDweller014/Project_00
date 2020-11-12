ffb5d347d170b2a2634314185431ee921efc1645
Mohammad Hamzah <39864689+ForestDweller014@users.noreply.github.com>
2020-11-12 01:02:49 -0500

    Update song_array.h

--- song_array.h
+++ song_array.h
@@ -5,4 +5,4 @@ void shuffle(struct song_node **library, int num);
 void print_artist_songs(struct song_node **library, char *artist);
 struct song_node *search_song(struct song_node **library, char *name, char *artist);
 void delete_song_from_lib (struct song_node **library, char* name, char* artist);
-struct song_node **clear_lib(struct song_node **library);
+void clear_lib(struct song_node **library);
